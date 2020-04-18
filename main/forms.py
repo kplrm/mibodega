93a3f6e19defbe1a79e84a778a1b4175c8b130fa
kplrm <guillen.diego@hotmail.es>
2020-04-19 00:17:38 +0200

    users register login logout

--- main/forms.py
+++ main/forms.py
@@ -1,42 +1,55 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 
 #from accounts.models import 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required = True, max_length=254)
+    first_name = forms.CharField(required = False)
+    last_name = forms.CharField(required = False)
+    
+    is_staff =  forms.BooleanField(required = False, widget=forms.HiddenInput())
+    is_active = forms.BooleanField(initial=True, required = False, widget=forms.HiddenInput())
+    is_bodega = forms.BooleanField(initial=False, required = False, widget=forms.HiddenInput())
+
+    phone = forms.IntegerField(required = True)
+    geolocation = forms.CharField(required = False, widget=forms.HiddenInput())
+    ID_listado = forms.IntegerField(required = False, widget=forms.HiddenInput())
 
     class Meta:
         model = User
         fields = ( # alll field names
             'username',
-            'first_name',
-            'last_name',
             'email',
             'password1',
-            'password2'
+            'password2',
+            'first_name',
+            'last_name',
+            'is_staff',
+            'is_active',
+            'is_bodega',
+            'phone',
+            'geolocation',
+            'ID_listado',
         )
+        widgets = {'geolocation': forms.HiddenInput()}
 
     def save(self, commit=True): #commit saves data to database
         user = super(RegistrationForm, self).save(commit=False) # when finish edition, it will store the data
+        user.username = self.cleaned_data['username']
+        user.email = self.cleaned_data['email']
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name'] # this is already in django model therefore user.
-        user.email = self.cleaned_data['email']
+
+        user.is_staff = self.cleaned_data['is_staff']
+        user.is_active = self.cleaned_data['is_active']
+        user.is_bodega = self.cleaned_data['is_bodega']
+        user.phone = self.cleaned_data['phone']
+        user.geolocation = self.cleaned_data['geolocation']
+        user.ID_listado = self.cleaned_data['ID_listado']
 
         if commit:
             user.save() # saves the data
 
         return user
-
-#class EditProfileForm(UserChangeForm):
-#    template_name='/something/else'
-#
-#    class Meta:
-#        model = User
-#        fields = (
-#            'email',
-#            'first_name',
-#            'last_name',
-#            'password'
-#        )
--- main/views.py
+++ main/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import render, redirect # to redirect the user
 from .models import productos_aprobados
 
 from .forms import RegistrationForm
-from django.contrib import messages
-from django.contrib.auth import login, logout, authenticate
-
-#from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages # to send unique messages to the users
 
 # Create your views here.
 def homepage(request):
@@ -19,10 +18,42 @@ def register(request):
     if request.method =='POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(reverse('main:homepage'))
-    else:
-        form = RegistrationForm()
+            user = form.save()
+            username = user.username # normalize to a standard format
+            # Messages are stored only once. When they are delivered, they also are deleted.
+            messages.success(request,f"Cuenta creada exitosamente") # (request, exact message)
+            auth_login(request, user)
+            messages.info(request,f"Bienvenido: {username}")
+            return redirect('main:homepage')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+            
+    form = RegistrationForm() # Rerender form
+    return render(request, 'main/register.html', context={"form":form})
 
-        args = {'form': form}
-        return render(request, 'main/register.html', args)
+def logout_request(request):
+    auth_logout(request)
+    messages.info(request, "Sessión cerrada. Vuelve pronto.")
+    return redirect("main:homepage")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.clean()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                print("exito")
+                auth_login(request, user)
+                messages.info(request,f"Bienvenido: {username}")
+                return redirect('main:homepage')
+            else:
+                messages.error(request,f"Usuario o contraseña incorrecta")
+        # in case the form is invalid
+        messages.error(request,f"Usuario o contraseña incorrecta")
+
+    form = AuthenticationForm()
+    return render(request, "main/login.html", {"form":form})
