1cd22dac6dd372dcbe958b301c6ac6a7a3f1e370
kplrm <guillen.diego@hotmail.es>
2020-04-20 18:30:05 +0200

    working new client structure

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
-#from accounts.models import 
+from .models import Cliente
 
 class RegistrationForm(UserCreationForm):
     class Meta:
@@ -10,48 +9,41 @@ class RegistrationForm(UserCreationForm):
         fields = ( # alll field names
             'username',
             'email',
-            'password1',
-            'password2',
             'first_name',
             'last_name',
-            'is_staff',
-            'is_active',
-            'is_bodega',
-            'phone',
-            'geolocation',
-            'ID_listado',
+            'password1',
+            'password2',
         )
-        widgets = {'geolocation': forms.HiddenInput()}
         
     email = forms.EmailField(required = True, max_length=254)
-    first_name = forms.CharField(required = False)
-    last_name = forms.CharField(required = False)
-    
-    is_staff =  forms.BooleanField(required = False, widget=forms.HiddenInput())
-    is_active = forms.BooleanField(initial=True, required = False, widget=forms.HiddenInput())
-    is_bodega = forms.BooleanField(initial=False, required = False, widget=forms.HiddenInput())
-
-    phone = forms.IntegerField(required = True)
-    geolocation = forms.CharField(required = False, widget=forms.HiddenInput())
-    ID_listado = forms.IntegerField(required = False, widget=forms.HiddenInput())
-
-
+    first_name = forms.CharField(required = True, max_length=254)
+    last_name = forms.CharField(required = True, max_length=254)
 
     def save(self, commit=True): #commit saves data to database
         user = super(RegistrationForm, self).save(commit=False) # when finish edition, it will store the data
         user.username = self.cleaned_data['username']
         user.email = self.cleaned_data['email']
         user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name'] # this is already in django model therefore user.
-
-        user.is_staff = self.cleaned_data['is_staff']
-        user.is_active = self.cleaned_data['is_active']
-        user.is_bodega = self.cleaned_data['is_bodega']
-        user.phone = self.cleaned_data['phone']
-        user.geolocation = self.cleaned_data['geolocation']
-        user.ID_listado = self.cleaned_data['ID_listado']
+        user.last_name = self.cleaned_data['last_name']
 
         if commit:
             user.save() # saves the data
 
         return user
+
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = (
+            'cl_phone',
+        )
+
+    cl_phone = forms.CharField(required=True,max_length=9)
+
+    def save(self, commit): #commit saves data to database
+        client = super(ClientForm, self).save(commit=False) # when finish edition, it will store the data
+        client.cl_phone = self.cleaned_data['cl_phone']
+
+        if commit:
+            client.save() # saves the data
+        return client
