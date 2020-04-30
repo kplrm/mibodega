09adda902fc4749f91f6b3491d02f5e86e821b0a
kplrm <guillen.diego@hotmail.es>
2020-05-01 01:38:52 +0200

    minor changes for publishing

--- main/urls.py
+++ main/urls.py
@@ -7,10 +7,10 @@ app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("register/", views.register, name="register"),
-    path("login/", views.login_request, name="login_request"),
-    path("logout/", views.logout_request, name="logout_request"),
+    path("register", views.register, name="register"),
+    path("login", views.login_request, name="login_request"),
+    path("logout", views.logout_request, name="logout_request"),
     #url(r'^add-to-cart/(?P<slug>\d{8,11}[\-].*)/$', cart_update, name="single"),
-    path("update/", views.cart_add, name="cart_add"),
-    path("remove/", views.remove_cart, name="remove_cart")
+    path("update", views.cart_add, name="cart_add"),
+    path("remove", views.remove_cart, name="remove_cart")
 ]
