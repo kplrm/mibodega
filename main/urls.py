5bc4f9d306486dfa3689ed612f7985f23bab49ff
kplrm <guillen.diego@hotmail.es>
2020-05-05 02:19:27 +0200

    minor changes

--- main/urls.py
+++ main/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     #url(r'^add-to-cart/(?P<slug>\d{8,11}[\-].*)/$', cart_update, name="single"),
     path("update", views.cart_add, name="cart_add"),
     path("remove", views.remove_cart, name="remove_cart"),
-    path("checkout", views.checkout, name="checkout")
+    path("checkout", views.checkout, name="checkout"),
+    path("select_shop", views.select_shop, name="select_shop")
 ]
