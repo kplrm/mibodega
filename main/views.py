22ab4fca7f8d324b172473b340de1afa3291a3da
kplrm <guillen.diego@hotmail.es>
2020-05-06 15:42:55 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -23,6 +23,7 @@ from ipregistry import IpregistryClient, NoCache
 MEDIA_URL = settings.MEDIA_URL
 
 def save_store_location(request):
+    print("hola")
     if request.is_ajax() and request.method== "POST":
         message = "Yes, AJAX!"
     else:
