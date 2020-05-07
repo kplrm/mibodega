85549be88032a687e4f26ccf1a0aff56c3e4856b
kplrm <guillen.diego@hotmail.es>
2020-05-07 20:25:49 +0200

    Update models.py

--- main/models.py
+++ main/models.py
@@ -64,7 +64,7 @@ class Cliente(models.Model):
     cl_phone = models.CharField(max_length=9,blank=False,null=True,verbose_name="Celular")
     cl_email = models.CharField(max_length=50,blank=True,null=True,verbose_name="E-mail")
     cl_address = models.CharField(max_length=50,blank=True,null=True,verbose_name="Dirreción")
-    cl_geolocation = models.PointField(blank=True,null=True,verbose_name="Dirección")
+    cl_geolocation = models.PointField(blank=True,null=True,verbose_name="Ubicación")
     cl_date_reg = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de registro")
     cl_bodega_ID = models.CharField(max_length=36,default="",blank=True,null=True,verbose_name="ID Bodega")
 
