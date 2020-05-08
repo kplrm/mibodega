54100d7fafc2356475e4d00234e7337fd76c019a
Diego Guillen <guillen.diego@hotmail.es>
2020-05-08 14:11:57 +0000

    last working master branch

--- mibodega/settings.py
+++ mibodega/settings.py
@@ -1,8 +1,8 @@
 import os
 import json
 
-#with open('/etc/conf.json') as config_file:
-#    config = json.load(config_file)
+with open('/etc/conf.json') as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
