bc304aba87a144ae055b00aa059db7fbbd719101
kplrm <guillen.diego@hotmail.es>
2020-05-09 16:32:18 +0200

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -7,19 +7,19 @@ from django.contrib.gis.admin import OSMGeoAdmin
 
 @admin.register(Cart)
 class CartAdmin(OSMGeoAdmin):
-    list_display = ('crt_ID','crt_user','crt_total_price','crt_ordered')
+    list_display = ('pk','crt_ID','crt_user','crt_total_price','crt_ordered')
 @admin.register(CartItem)
 class CartItemAdmin(OSMGeoAdmin):
     list_display = ('pk','ci_cart_ID','ci_user','ci_product','ci_quantity')
 @admin.register(ProductosEnBodega)
 class ProductosEnBodegaAdmin(OSMGeoAdmin):
-    list_display = ('peb_bodega','peb_product','peb_status','peb_discount_status')
+    list_display = ('pk','peb_bodega','peb_product','peb_status','peb_discount_status')
 @admin.register(ProductosAprobados)
 class ProductosAprobadosAdmin(OSMGeoAdmin):
-    list_display = ('pa_product','pa_category','pa_brand')
+    list_display = ('pk','pa_product','pa_category','pa_brand')
 @admin.register(Cliente)
 class ClienteAdmin(OSMGeoAdmin):
-    list_display = ('cl_user','cl_first_name','cl_last_name','cl_phone','cl_address','cl_geolocation')
+    list_display = ('pk','cl_user','cl_first_name','cl_last_name','cl_phone','cl_address','cl_geolocation')
 @admin.register(Bodega)
 class BodegaAdmin(OSMGeoAdmin):
-    list_display = ('bd_ID','bd_user','bd_is_active','bd_name','bd_ruc','bd_raz_soc','bd_geolocation')
+    list_display = ('pk','bd_ID','bd_user','bd_is_active','bd_name','bd_ruc','bd_raz_soc','bd_geolocation')
