7372d079712a62c5dc82b3a97372fe945c291443
kplrm <guillen.diego@hotmail.es>
2020-05-13 22:59:19 +0200

    Update settings.py

--- mibodega/settings.py
+++ mibodega/settings.py
@@ -128,6 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #EMAIL_HOST_PASSWORD = config['EMAIL_PASSWORD']
 #EMAIL_PORT = 465
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.zoho.eu'
 EMAIL_HOST_USER = 'hola@alimentos.pe'
 EMAIL_HOST_PASSWORD = 'C4mp30n123.'
