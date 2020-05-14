dcb17e356ac9847c4cc85bb09621079e7ff0f7db
kplrm <guillen.diego@hotmail.es>
2020-05-14 22:59:05 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -652,10 +652,10 @@ def send_order_mail(orders_obj,usr_first,usr_last,usr_street,usr_geolocation,usr
     #encodedStr = str(encodedBytes, "utf-8")
     print("response")
     print(response)
-    print("image")
-    print(img)
+    #print("image")
+    #print(img)
     print("encoded image:")
-    #print(encodedStr)
+    print(usr_map)
 
 
     context = {
