51ae97ffdac8f12921a6419f92e61b1649f5cbf7
kplrm <guillen.diego@hotmail.es>
2020-05-15 13:17:43 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def dashboard(request):
     return render(request=request, # to reference request
                   template_name="dashboard/index.html", # where to find the specifix template
-                  context={})
+                  context={'ver': 0})
