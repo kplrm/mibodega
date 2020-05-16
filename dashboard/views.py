c13dea60d11c744966e177d76dcbd82826b794c6
kplrm <guillen.diego@hotmail.es>
2020-05-16 10:29:14 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 def dashboard(request):
     if request.user.is_authenticated:
         print("user is authenticated")
+        cliente = Cliente.objects.all().filter(cl_user=request.user).first()
+        print("cliente")
     else:
         print("user is NOT authenticated")
     return render(request=request,
