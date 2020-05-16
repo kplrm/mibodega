a96dcaee33ea05a48cbdad75b89901d6c3b0643b
kplrm <guillen.diego@hotmail.es>
2020-05-16 19:15:24 +0200

    greetings msg added

--- dashboard/views.py
+++ dashboard/views.py
@@ -47,7 +47,8 @@ def dashboard(request):
         if cliente.cl_is_bodega:
             context = {
                         'BodegaDashboard_obj': BodegaDashboard_obj,
-                        'cliente': cliente
+                        'cliente': cliente,
+                        'bodega': bodega
                     }
             return render(request=request,template_name="dashboard/index.html",context=context)
         else:
