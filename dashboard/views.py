d8172d11825517de147c335fd663c4249b2115b5
kplrm <guillen.diego@hotmail.es>
2020-05-16 21:17:55 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -121,6 +121,6 @@ def find_most_sold_products(OrderItem_list, list_size):
                     str(item.oi_id_product): int(item.oi_quantity)
                 })
     most_sold_products = sorted(most_sold_products.items(), key=lambda x: x[1], reverse=True)
-    for x in list(most_sold_products)[0:list_size]:
+    for x in list(most_sold_products):
         print (most_sold_products[x])
     return most_sold_products
