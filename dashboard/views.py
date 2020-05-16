1cb137a725ad883a82520ffad6fbce80cf63a200
kplrm <guillen.diego@hotmail.es>
2020-05-16 23:30:45 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -119,10 +119,10 @@ def find_most_sold_products(OrderItem_list):
     for item in OrderItem_list:
         if item.oi_date_created.date() > (date.today()+timedelta(days = -30)):
             if item.oi_id_product in most_sold_products:
-                most_sold_products[str(item.oi_id_product)] += int(item.oi_quantity)
+                most_sold_products[str(item.oi_id_product)][0] += int(item.oi_quantity)
             else:
                 most_sold_products.update({
-                    str(item.oi_id_product): int(item.oi_quantity)
+                    str(item.oi_id_product): ( int(item.oi_quantity), str(item.oi_product) )
                 })
     most_sold_products = sorted(most_sold_products.items(), key=lambda x: x[1], reverse=True)
     # In case adding enumeration is needed
