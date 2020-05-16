6998cd3c6056610d4d1527cd94632d4617138c9c
kplrm <guillen.diego@hotmail.es>
2020-05-16 09:41:35 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -640,7 +640,7 @@ def send_order_mail(orders_obj,bodegas,usr_first,usr_last,usr_street,usr_geoloca
             bodegas_en_cesta.update({str(product.oi_id_bodega):str(product.oi_ruc_bodega)})
             bodega_names.update({str(product.oi_id_bodega):str(product.oi_bodega_name)})
             bodega_phones.update({str(product.oi_id_bodega):str(product.oi_bodega_phone)})
-            subtotal_bodegas.update({str(product.oi_id_bodega):float(product.oi_price) * float(product.oi_quantity) })
+            #subtotal_bodegas.update({str(product.oi_id_bodega):float(product.oi_price) * float(product.oi_quantity) })
             subtotal_bodegas.update({str(product.oi_id_bodega):bodegas[str(product.oi_id_bodega)] })
 
     # usr_geolocation with regex
