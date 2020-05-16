63f58cf19a531192cf7b2772173aa75347650424
kplrm <guillen.diego@hotmail.es>
2020-05-16 14:13:33 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -62,7 +62,7 @@ def update_values_BodegaDashboard(BodegaDashboard_obj, BodegaOrders_list, OrderI
     today_sales = 0
     for order in BodegaOrders_list:
         print("Today: ", date.today())
-        print("order.bo_date_created", order.bo_date_created.strftime('%y-%m-%d'))
+        print("order.bo_date_created", order.bo_date_created.strftime('%Y-%m-%d'))
         if order.bo_date_created == date.today():
             today_sales += order.bo_total_price
     
