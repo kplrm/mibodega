5862f8f6d96bd9578394dbeb3282f7a9f16724a0
kplrm <guillen.diego@hotmail.es>
2020-05-16 11:02:16 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,16 +10,20 @@ def dashboard(request):
         cliente = Cliente.objects.all().filter(cl_user=request.user).first()
         print(cliente)
         # Render only if it's bodega
+        # To avoid any rendering or calculation if it's not a bodega
+        if cliente.cl_is_bodega == False:
+            print("first exit")
+            return render(request=request,
+                  template_name="dashboard/index.html")
+
         if cliente.cl_is_bodega:
             return render(request=request,
                   template_name="dashboard/index.html")
         else:
+            print("second exit")
             return HttpResponseRedirect(reverse('main:homepage'))
 
     else:
         print("user is NOT authenticated")
-        #return render(request=request,
-        #          template_name="main/index.html")
-        #return HttpResponseRedirect(reverse('https://www.google.de/'))
-        return None
+        return HttpResponseRedirect(reverse('main:homepage'))
                   
