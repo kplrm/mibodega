ecc35d9508e9e1f6466c79c0df4f0554230cad37
kplrm <guillen.diego@hotmail.es>
2020-05-16 12:04:21 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from main.models import Cliente, Bodega, OrderItem, BodegaOrders
