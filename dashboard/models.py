8d5415752569248a65e7919f7163dfe85e310e07
kplrm <guillen.diego@hotmail.es>
2020-05-16 14:28:20 +0200

    minor changes

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,8 +5,9 @@ from main.models import Bodega, Cliente
 class BodegaDashboard(models.Model):
     bd_ID = models.OneToOneField(Bodega,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
     bd_user = models.ForeignKey(Cliente,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
-    bd_daily_sales = models.DecimalField(default=0.00,max_digits=6,decimal_places=2,blank=True,null=True)
-    bd_weekly_sales = models.DecimalField(default=0.00,max_digits=6,decimal_places=2,blank=True,null=True)
+    bd_daily_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
+    bd_weekly_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
+    bd_monthly_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
 
     def __str__(self):
         return str(self.bd_ID)
