5ffa621d3de9da80971399d99c6e39a8dd6a5552
kplrm <guillen.diego@hotmail.es>
2020-05-16 10:33:38 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,7 +5,7 @@ def dashboard(request):
     if request.user.is_authenticated:
         print("user is authenticated")
         cliente = Cliente.objects.all().filter(cl_user=request.user).first()
-        print("cliente")
+        print(cliente)
     else:
         print("user is NOT authenticated")
     return render(request=request,
