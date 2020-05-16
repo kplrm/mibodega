61628dcd2cad1e97005bf323fb11601aea4ce02e
kplrm <guillen.diego@hotmail.es>
2020-05-16 13:15:08 +0200

    changing timezone

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,7 +3,7 @@ from main.models import Bodega, Cliente
 
 # Always DecimalField for money because float has rounding issues
 class BodegaDashboard(models.Model):
-    bd_ID = models.ForeignKey(Bodega,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
+    bd_ID = models.OneToOneField(Bodega,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
     bd_user = models.ForeignKey(Cliente,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
     bd_daily_sales = models.DecimalField(default=0.00,max_digits=6,decimal_places=2,blank=True,null=True)
     bd_weekly_sales = models.DecimalField(default=0.00,max_digits=6,decimal_places=2,blank=True,null=True)
