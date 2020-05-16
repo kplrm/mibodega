fbddf243bdc0876d480e909d74ea1a29ac0bf040
kplrm <guillen.diego@hotmail.es>
2020-05-16 17:25:45 +0200

    testing last values

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,7 +5,8 @@ from main.models import Bodega, Cliente
 class BodegaDashboard(models.Model):
     bd_ID = models.OneToOneField(Bodega,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
     bd_user = models.ForeignKey(Cliente,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
-    bd_last_update = models.DateTimeField(default=None,blank=True,null=True,verbose_name="¿Last update")
+    bd_last_update = models.DateTimeField(default=None,blank=True,null=True,verbose_name="¿Last update")    
+
     bd_daily_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
     bd_weekly_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
     bd_monthly_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
