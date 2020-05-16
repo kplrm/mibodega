995cf35f495a3f7715d8ba2598963e9e3d089b0b
kplrm <guillen.diego@hotmail.es>
2020-05-16 17:04:38 +0200

    adding debugging time created to orderitems

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,12 +5,16 @@ from main.models import Bodega, Cliente
 class BodegaDashboard(models.Model):
     bd_ID = models.OneToOneField(Bodega,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
     bd_user = models.ForeignKey(Cliente,null=True,on_delete=models.CASCADE,verbose_name="Usuario")
+    bd_last_update = models.DateTimeField(default=None,blank=True,null=True,verbose_name="¿Last update")
     bd_daily_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
     bd_weekly_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
     bd_monthly_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
     bd_last_day_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
     bd_last_week_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
     bd_last_month_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
+    bd_daily_change_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
+    bd_weekly_change_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
+    bd_monthly_change_sales = models.DecimalField(default=0.00,max_digits=10,decimal_places=2,blank=True,null=True)
 
     def __str__(self):
         return str(self.bd_ID)
