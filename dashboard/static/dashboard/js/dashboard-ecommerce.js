3f15553dd183382a953146d4c97b06da61636231
kplrm <guillen.diego@hotmail.es>
2020-05-17 13:08:09 +0200

    Update dashboard-ecommerce.js

--- dashboard/static/dashboard/js/dashboard-ecommerce.js
+++ dashboard/static/dashboard/js/dashboard-ecommerce.js
@@ -51,7 +51,8 @@
                     var dataset = data['datasets'][0];
                     var percent = Math.round((dataset['data'][tooltipItem['index']] / sales_volumen_last_30));
                     console.log(dataset);
-                    console.log(tooltipItem['index']);
+                    console.log("tooltipItem['index']: "+tooltipItem['index']);
+                    console.log("math: "+(dataset['data'][tooltipItem['index']]));
                     console.log(percent);
                     console.log(sales_volumen_last_30);
                     return '(' + percent + '%)';
