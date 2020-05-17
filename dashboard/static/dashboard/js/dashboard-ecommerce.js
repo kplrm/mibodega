d1c5461cc0768415e49fbf81c4ce3d20a3e53cfc
kplrm <guillen.diego@hotmail.es>
2020-05-17 13:23:03 +0200

    working tooltip with percentage in label

--- dashboard/static/dashboard/js/dashboard-ecommerce.js
+++ dashboard/static/dashboard/js/dashboard-ecommerce.js
@@ -49,18 +49,13 @@
             callbacks: {
                 label: function(tooltipItem, data) {
                     var label = data['labels'][tooltipItem['index']];
-                    console.log(label);
                     if (label) {
                         label += ': ';
                     }
-                    console.log(label);
                     var dataset = data['datasets'][0];
                     var temp = (dataset['data'][tooltipItem['index']]/sales_volumen_last_30)*10000;
                     var percent = Math.round(temp)/100;
-                    var temp2 = '(' + percent + '%)';
                     label += '(' + percent + '%)';
-                    console.log(label);
-                    console.log(label+temp2);
                     return label;
                   }
             }
