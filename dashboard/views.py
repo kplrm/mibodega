6c18590c56d4853638273862c994fc5e38a02a0a
kplrm <guillen.diego@hotmail.es>
2020-05-17 16:07:50 +0200

    added products page

--- dashboard/views.py
+++ dashboard/views.py
@@ -47,9 +47,64 @@ def dashboard(request):
         else:
             top10_products = list(most_sold_products)[0:top_list_size]
         most_sold_products = list(most_sold_products)
-        #top10_products = list(most_sold_products)[0:top_list_size] #for debugging
 
-        
+        ################################# PAGE CONTENT END #################################
+        ####################################################################################
+        # Second check in the footer to render only if cl_is_bodega, and avoid None or any other value
+        if cliente.cl_is_bodega:
+            context = {
+                        'BodegaDashboard_obj': BodegaDashboard_obj,
+                        'OrderItem_list': OrderItem_list,
+                        'cliente': cliente,
+                        'bodega': bodega,
+                        'most_sold_products': most_sold_products,
+                        'top10_products': top10_products
+                    }
+            return render(request=request,template_name="dashboard/index.html",context=context)
+        else:
+            return HttpResponseRedirect(reverse('main:homepage'))
+
+    else:
+        return HttpResponseRedirect(reverse('main:homepage'))
+
+def productos(request):
+    if request.user.is_authenticated:
+        cliente = Cliente.objects.all().filter(cl_user=request.user).first()
+        # Render only if it's bodega
+        # To avoid any rendering or calculation if it's not a bodega
+        if cliente.cl_is_bodega == False:
+            return HttpResponseRedirect(reverse('main:homepage'))
+        ####################################################################################
+        ################################### PAGE CONTENT ###################################
+        # Search for client's bodega and it's data
+        bodega = Bodega.objects.all().filter(bd_ID=cliente.cl_default_bodega).first()
+        BodegaDashboard_obj, created = BodegaDashboard.objects.get_or_create(bd_ID=bodega,bd_user=cliente)
+        print("created? ", created)
+
+        # Find BodegaOrders with their corresponding OrderItem
+        BodegaOrders_list = get_list_or_404(BodegaOrders,bo_bodega=bodega)
+        OrderItem_list = []
+        for bodega_order in BodegaOrders_list:
+            item_list = get_list_or_404(OrderItem,oi_bo_ID=bodega_order,oi_is_anulado=False) # Take out the 'anulados'
+            for item in item_list:
+                if item in OrderItem_list:
+                    pass
+                else:
+                    OrderItem_list.append(item)
+        print("BodegaOrders_list? ", len(BodegaOrders_list))
+        print("OrderItem_list? ", len(OrderItem_list))
+
+        # Update BodegaDashboard values
+        update_values_BodegaDashboard(BodegaDashboard_obj, BodegaOrders_list)
+
+        # Find the most sold products
+        most_sold_products = find_most_sold_products(OrderItem_list)
+        top_list_size = 2 #10
+        if len(most_sold_products) > top_list_size:
+            top10_products = list(most_sold_products)[0:len(most_sold_products)]
+        else:
+            top10_products = list(most_sold_products)[0:top_list_size]
+        most_sold_products = list(most_sold_products)
 
         ################################# PAGE CONTENT END #################################
         ####################################################################################
