fe1b3a23ba42da793b97aa71ca366f827cfc8684
kplrm <guillen.diego@hotmail.es>
2020-05-17 23:32:34 +0200

    testing valid form

--- dashboard/static/dashboard/js/product_detail.js
+++ dashboard/static/dashboard/js/product_detail.js
@@ -1,10 +1,37 @@
 // JS for productos template
-function check_format(input) {
-    var input_value = input.value;
-    input.value = input_value.toFixed(2);
-};
+function submit_checkout() {
+    if $("#products_to_update").validate().form() {
+        alert("Form is valid");
+    } else {
+        alert("Form is NOT valid");
+    };
 
+/*    if ($("#checkout_form").validate().form() && {{cart_obj.crt_total_price}} != 0 && usr_geolocation != "") {
+        // submit with AJAX
+        $.ajax({
+            headers: {'X-CSRFToken': '{{ csrf_token }}'},
+            url: "/submit_checkout",
+            type: "POST",
+            dataType: "json",
+            data:   {
+                        'csrfmiddlewaretoken': csrftoken,
+                        'cart_obj_id': cart_obj_id.toString(),
+                        'usr_first': usr_first,
+                        'usr_last': usr_last,
+                        'usr_street': usr_street,
+                        'usr_geolocation': usr_geolocation,
+                        'usr_email': usr_email,
+                        'usr_phone': usr_phone,
+                        'usr_comments': usr_comments,
+                        // spare line to send confirmation if user wants to update his preferences
+                    }
+        });
+    } else {
+        alert("Seleccione en el mapa su ubicación");
+    }*/
+};
 
 $(document).ready(function(){
 
 });
+
