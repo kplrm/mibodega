5666b701b3f71de2c7fbb63e83a893dc42327784
kplrm <guillen.diego@hotmail.es>
2020-05-17 12:12:36 +0200

    testing adding percentage

--- dashboard/static/dashboard/js/dashboard-ecommerce.js
+++ dashboard/static/dashboard/js/dashboard-ecommerce.js
@@ -45,6 +45,23 @@
     };
 
     options = {
+        tooltips: {
+            enabled: false
+        },
+        plugins: {
+            datalabels: {
+                formatter: (value, ctx) => {
+                    let sum = 0;
+                    let dataArr = ctx.chart.data.datasets[0].data;
+                    dataArr.map(data => {
+                        sum += data;
+                    });
+                    let percentage = (value*100 / sum).toFixed(2)+"%";
+                    return percentage;
+                },
+                color: '#fff',
+            }
+        }
         
     };
 
