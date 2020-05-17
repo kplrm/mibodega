3f42750062cb23679b9b343d13cc4ddb4e8f16dc
kplrm <guillen.diego@hotmail.es>
2020-05-17 13:16:12 +0200

    Update dashboard-ecommerce.js

--- dashboard/static/dashboard/js/dashboard-ecommerce.js
+++ dashboard/static/dashboard/js/dashboard-ecommerce.js
@@ -47,37 +47,12 @@
     var options = {
         tooltips: {
             callbacks: {
-                afterLabel: function(tooltipItem, data) {
+                label: function(tooltipItem, data) {
                     var dataset = data['datasets'][0];
                     var temp = (dataset['data'][tooltipItem['index']]/sales_volumen_last_30)*10000;
                     var percent = Math.round(temp)/100;
-                    console.log(dataset);
-                    console.log("tooltipItem['index']: "+tooltipItem['index']);
-                    console.log("math: "+ dataset['data'][tooltipItem['index']] );
-                    console.log("sales_volumen_last_30: "+sales_volumen_last_30);
-                    console.log(dataset['data'][tooltipItem['index']]/sales_volumen_last_30*100);
-                    
                     return '(' + percent + '%)';
                   }
-                //label: function(tooltipItem, data) {
-                    //var label = data.datasets[tooltipItem.datasetIndex].label || '';
-                    //var label = "Laikitas";
-                    //if (label) {
-                    //    label += ': ';
-                    //}
-                    //label += "hola";
-                    // Calculate total volume
-                    /*
-                    var sales_volumen = 0
-                    for(var i = 0, size = series_most_sold_products.length; i < size ; i++){
-                        var item = series_most_sold_products[i];
-                        sales_volumen += item;
-                    }
-                    if (label) {
-                        label += sales_volumen;
-                    }*/
-                    //return label;
-                //}
             }
         }
     };
