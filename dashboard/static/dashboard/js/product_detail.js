d83c3edeb6150f1046425d178424c824c66d7550
kplrm <guillen.diego@hotmail.es>
2020-05-17 23:14:47 +0200

    review input format test

--- dashboard/static/dashboard/js/product_detail.js
+++ dashboard/static/dashboard/js/product_detail.js
@@ -1,4 +1,10 @@
-// Update product ProductosEnBodega
+// JS for productos template
+function check_format(input) {
+    var input_value = input.value;
+    input.value = input_value.toFixed(2);
+};
+
+
 $(document).ready(function(){
 
 });
