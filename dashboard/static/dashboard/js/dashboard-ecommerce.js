3134291477bf0a75e8a0955117a0d11f5fd78db8
kplrm <guillen.diego@hotmail.es>
2020-05-17 03:12:47 +0200

    Update dashboard-ecommerce.js

--- dashboard/static/dashboard/js/dashboard-ecommerce.js
+++ dashboard/static/dashboard/js/dashboard-ecommerce.js
@@ -36,10 +36,10 @@
     // Ventas por producto en los últimos 30 días
     // ============================================================== 
     var chart = new Chartist.Pie('.ct-chart-category', {
-        //series: [60, 30, 30],
-        //labels: ['Bananas', 'Apples', 'Grapes']
-        series: series_most_sold_products,
-        labels: labels_most_sold_products
+        series: [60, 30, 30],
+        labels: ['Bananas', 'Apples', 'Grapes']
+        //series: series_most_sold_products,
+        //labels: labels_most_sold_products
     }, {
         donut: true,
         showLabel: false,
