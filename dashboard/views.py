9ba7fa5e471fb354e1742e24bf43f175ccc9f8f2
kplrm <guillen.diego@hotmail.es>
2020-05-18 08:44:45 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -216,9 +216,12 @@ def find_most_sold_products(OrderItem_list):
 
 def save_product_changes(request):
     if request.method== "POST" and request.is_ajax():
-        print("yups a post")
+        print("Working POST")
         changes = request.POST.get('changes',False)
-        print(changes)
+        for product_changes in changes:
+            print"(============")
+            print(product_changes)
+
         #changes = request.POST['changes']
         #print(changes)
 #        cart_obj = Cart.objects.all().filter(crt_ID=cart_obj_id).first()
