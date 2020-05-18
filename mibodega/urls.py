698d99af17bbe6244f12b5d5f6a93276a76f6ef0
kplrm <guillen.diego@hotmail.es>
2020-05-18 21:27:15 +0200

    Update urls.py

--- mibodega/urls.py
+++ mibodega/urls.py
@@ -9,7 +9,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('main.urls')),
-    path("dashboard/", include('mibodega.dashboard.urls')),
 ]
 
 if settings.DEBUG:
