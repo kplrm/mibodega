15594658393b1139080589f22d5150134f5f99e9
kplrm <guillen.diego@hotmail.es>
2020-05-19 18:12:05 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -975,7 +975,7 @@ def productos(request):
         # Get all missing productos aprobados
         #try:
         print("trying...")
-        ProductosAprobados_all = ProductosAprobados.objects.all().filter(pa_status=True).first()
+        ProductosAprobados_all = ProductosAprobados.objects.all().filter(pa_status=True)
         print(ProductosAprobados_all)
         ProductosAprobados_missing = []
         for producto_aprobado in ProductosAprobados_all:
