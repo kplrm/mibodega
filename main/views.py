320c4fbe5a71dbfdd774cc88671fd42e09c48eaf
kplrm <guillen.diego@hotmail.es>
2020-05-21 04:29:59 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -714,6 +714,7 @@ def submit_checkout(request):
             if item.ci_product.peb_status == False:
                 print("delete object")
                 peb = ProductosEnBodega.objects.all().filter(peb_ID=item.ci_product.peb_ID).all()
+                print("step 1")
                 cart_obj.crt_product.remove(peb) # remove crt_product
                 cart_obj.crt_item.remove(item) # remove crt_item
                 item.delete()
