59c9b7911b3457c14242ced6e1783b3e0af6196e
kplrm <guillen.diego@hotmail.es>
2020-05-31 16:46:19 +0200

    Update forms.py

--- main/forms.py
+++ main/forms.py
@@ -126,7 +126,13 @@ class ClientForm(forms.ModelForm):
             'cl_phone',
         )
 
-    cl_phone = forms.CharField(required=True,max_length=9)
+    #cl_phone = forms.CharField(required=True,max_length=9)
+    cl_phone = forms.CharField(
+        label=_("Celular"),
+        strip=False, # False = do not strip white spaces
+        max_length=9,
+        required = True,
+    )
 
     def save(self, commit): #commit saves data to database
         client = super(ClientForm, self).save(commit=False) # when finish edition, it will store the data
