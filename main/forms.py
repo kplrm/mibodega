5c5043dabb29e67ed4e54ddcdecca2fd05bf301d
kplrm <guillen.diego@hotmail.es>
2020-05-31 16:26:51 +0200

    Update forms.py

--- main/forms.py
+++ main/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from .models import Cliente, Cart
 
 from django import forms
+from django.contrib.auth import authenticate, get_user_model, password_validation
 
 class UserCreationForm(forms.ModelForm):
     error_messages = {
