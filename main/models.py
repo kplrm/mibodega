33a1fa8a4a2780653ab73442ad30b48fbf7cd602
kplrm <guillen.diego@hotmail.es>
2020-06-01 21:30:48 +0200

    Update models.py

--- main/models.py
+++ main/models.py
@@ -124,7 +124,7 @@ class ProductosEnBodega(models.Model):
             pass
 
         print("self.peb_regular_price:", self.peb_regular_price)
-        if (float(self.peb_regular_price)!=0) and (float(self.peb_discount_price)!=0) and self.peb_discount_price!=None:# and isinstance(self.peb_discount_price,float):
+        if (float(self.peb_regular_price)>0) and (float(self.peb_discount_price)!=0) and self.peb_discount_price!=None:# and isinstance(self.peb_discount_price,float):
             self.peb_discount_rate = (float(self.peb_discount_price)-float(self.peb_regular_price))/float(self.peb_regular_price)*100
         else:
             self.peb_discount_rate = 0
