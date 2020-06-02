62843b5184c153c94fd1d2cbd5cfd06060d1e316
kplrm <guillen.diego@hotmail.es>
2020-06-02 19:06:59 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -629,13 +629,16 @@ def checkout(request):
         def comparator_price( tupleElem ):
             print("tupleElem[1][0]: ", tupleElem[1][0])
             return tupleElem[1][0]
-        bodegas_with_products = sorted(bodegas_with_products.items(), key=comparator_price, reverse=False) # reverse=False -> Lowest to highest
+        #bodegas_with_products = sorted(bodegas_with_products.items(), key=comparator_price, reverse=False) # reverse=False -> Lowest to highest
+        bodegas_with_products.sort(key=comparator_price)
         print("bodegas_with_products: ",bodegas_with_products)
         # Most products on top
         def comparator_len( tupleElem ):
             print("tupleElem[1][1]: ", tupleElem[1][1])
             return tupleElem[1][1]
-        bodegas_with_products = sorted(bodegas_with_products.items(), key=comparator_len, reverse=True)
+        #bodegas_with_products = sorted(bodegas_with_products.items(), key=comparator_len, reverse=True)
+        bodegas_with_products.sort(key=comparator_len)
+        print("")
         print("bodegas_with_products: ",bodegas_with_products)
 
     except:
