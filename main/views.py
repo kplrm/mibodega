f100279e8f88d3244d8382d6f86b8307c4e56023
kplrm <guillen.diego@hotmail.es>
2020-06-04 17:58:59 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -664,7 +664,7 @@ def checkout(request):
             else: # No store has all items
                 for item in result[1][3]:
                     print("item: ",item)
-                    missin_items_list = cart_list.filter(~Q(ci_product__peb_product__pa_ID=item.ci_product.peb_product.pa_ID))
+                    #missin_items_list = cart_list.filter(~Q(ci_product__peb_product__pa_ID=item.ci_product.peb_product.pa_ID))
                     print("missin_items_list: ",missin_items_list)
                 pass
 
