8711486690dc3a004d0e09772604a18492ca4e2d
kplrm <guillen.diego@hotmail.es>
2020-06-05 00:38:03 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -661,7 +661,7 @@ def checkout(request):
             #print("tupleElem[1][1]: ", tupleElem[1][1])
             return tupleElem[1][1]
         bodegas_w_products_w_delivery.sort(key=comparator_len, reverse=True)
-        print("bodegas_w_products_w_delivery: ",bodegas_w_products_w_delivery)
+        #print("bodegas_w_products_w_delivery: ",bodegas_w_products_w_delivery)
 
         # Shop selection results
         result_list = []
