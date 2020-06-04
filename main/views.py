d5ef69922837d6b8d88374378e20e6daeaadefa5
kplrm <guillen.diego@hotmail.es>
2020-06-04 23:28:44 +0200

    m

--- main/views.py
+++ main/views.py
@@ -669,7 +669,7 @@ def checkout(request):
             # If all items are available at one store
             if result[1][1] == len(cart_list):
                 print("All items in store")
-                result_list.append([result,1])
+                result_list.append([result[0],result,1])
             # If items are available only buying at two shops
             else:
                 print("Missing items in ",result[1][2])
@@ -688,7 +688,7 @@ def checkout(request):
                     })
                 second_bodega_w_products_w_delivery = sorted(second_bodega_w_products_w_delivery.items(), key=comparator_price, reverse=False)
                 second_bodega_w_products_w_delivery.sort(key=comparator_len, reverse=True)
-                result_list.append([result,second_bodega_w_products_w_delivery[0]])
+                result_list.append([result[0]+second_bodega_w_products_w_delivery[0][0],result,second_bodega_w_products_w_delivery[0]])
                 #result_list.append(tuple(result,second_bodega_w_products_w_delivery[0]))
         print("######################")
         for result in result_list:
