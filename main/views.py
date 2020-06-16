4415ff941b5d765bdfb62eab0f7b0f0bf3ce4ad0
kplrm <guillen.diego@hotmail.es>
2020-06-16 03:48:16 +0200

    m

--- main/views.py
+++ main/views.py
@@ -1798,6 +1798,8 @@ def see_search_results(request):
             user_location = Point(float(user_longitude),float(user_latitude),srid=4326)
 
         # Retrieves search_text
+        search_text = request.POST.get('search-product',False)
+        print("search-product: ",search-product)
         search_text = request.POST.get('search_text',False)
         print("search_text: ",search_text)
         # Get search words
