7b6499b4cb00d59218d3c53dddbdf2571462734e
kplrm <guillen.diego@hotmail.es>
2020-06-26 19:55:36 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1221,7 +1221,7 @@ def registroBodega(request):
                 # Envio de email al cliente
                 email_message = EmailMultiAlternatives(subject=subject, text_content="Es hora de aumentar tus ventas online.", 
                                                         from_email="hola@alimentos.pe", to=[client.cl_email], bcc=["hola@alimentos.pe"])
-                email_message.attach_alternative(text_content=plain_message, "text/html")
+                email_message.attach_alternative(html_content, "text/html")
                 email_message.send()
 
                 return redirect('main:homepage')
