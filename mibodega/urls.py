e985498aa5f7aead553039de9169af7edcd46aaa
kplrm <guillen.diego@hotmail.es>
2020-06-27 17:21:35 +0200

    Update urls.py

--- mibodega/urls.py
+++ mibodega/urls.py
@@ -7,13 +7,15 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 #
 
+reset_subject = "Reestablece tu contraseña de Alimentos.pe"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('main.urls')),
 
     # Password Reset View
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"), name='password_reset'),
-    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_done.html"), name='password_reset_done'),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_done.html", subject_template_name=reset_subject), name='password_reset_done'),
     path("password_reset_confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_confirm.html"), name='password_reset_confirm'),
     path("password_reset_complete/done/", auth_views.PasswordResetCompleteView.as_view(template_name="main/password_reset_complete.html"), name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
