93a77d588c5012954594a7ed842751b29f846cd1
kplrm <guillen.diego@hotmail.es>
2020-06-27 15:49:43 +0200

    m

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, re_path
+from django.urls import path
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views as auth_views
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -18,11 +17,7 @@ urlpatterns = [
     path("login/", views.login_request, name="login_request"),
     path("logout/", views.logout_request, name="logout_request"),
     path("change_password/", views.change_password_request, name="change_password_request"),
-    # Password Reset View
-    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"), name='password_reset'),
-    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_done.html"), name='password_reset_done'),
-    path("password_reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_confirm.html"), name='password_reset_confirm'),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="main/password_reset_complete.html"), name='password_reset_complete'),
+    
 
     #path("password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/", auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_confirm.html"), name='password_reset_confirm'),
     #url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
