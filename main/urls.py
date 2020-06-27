edc50f00346454ccbc12d72683796418f1cd9a65
kplrm <guillen.diego@hotmail.es>
2020-06-27 15:28:51 +0200

    m

--- main/urls.py
+++ main/urls.py
@@ -21,11 +21,11 @@ urlpatterns = [
     # Password Reset View
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"), name='password_reset'),
     path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_done.html"), name='password_reset_done'),
-    path("password_reset_confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_confirm.html"), name='auth_password_reset_done'),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_confirm.html"), name='password_reset_confirm'),
     #path("password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/", auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_confirm.html"), name='password_reset_confirm'),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="main/password_reset_complete.html"), name='password_reset_complete'),
 
     #url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    #url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
     # Store front views and functions
     path("embutidos", views.embutidos, name="embutidos"),
