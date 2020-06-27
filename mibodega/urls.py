eeb2aa274d731cfad7f4c100326dc2a4d1ab0d1f
kplrm <guillen.diego@hotmail.es>
2020-06-27 15:49:43 +0200

    m

--- mibodega/urls.py
+++ mibodega/urls.py
@@ -4,11 +4,18 @@ from django.urls import path, include
 #DG
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 #
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('main.urls')),
+
+    # Password Reset View
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"), name='password_reset'),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_done.html"), name='password_reset_done'),
+    path("password_reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_confirm.html"), name='password_reset_confirm'),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="main/password_reset_complete.html"), name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #if settings.DEBUG:
